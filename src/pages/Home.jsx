9908938c9bb8056eebc5186556f27918b5122251
tomikorzu <tomykorzu@icloud.com>
2024-10-02 14:10:27 -0300

    feat(Home.jsx): add Backdrop applyment and comment it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,26 @@
 import { useEffect } from "react";
 import Loading from "../components/Loading.jsx";
 import Navbar from "../components/Navbar.jsx";
-import Backdrop from "../components/Backdrop.jsx";
+import { applyBlur } from "../utils/mainFunctions.js";
+
 const Home = () => {
   useEffect(() => {
     document.title = "Home - PokeAPI";
+    // const btn = document.getElementById("btn");
+    // btn.addEventListener("click", applyBlur);
   });
   return (
     <div className="fade-in">
       {/* <Loading /> */}
-      {/* <Backdrop /> */}
-      <Navbar />
-      <main>
-        <h1>Home</h1>
-      </main>
+      {/* <button className="btn" id="btn">
+        click
+      </button> */}
+      <div className="back-drop">
+        <Navbar />
+        <main>
+          <h1>Home</h1>
+        </main>
+      </div>
     </div>
   );
 };
