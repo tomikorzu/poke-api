9c4931547cd0ce84e924fac1f2e218f560008af3
tomikorzu <tomykorzu@icloud.com>
2024-10-02 13:22:05 -0300

    feat(Backdrop.jsx): add the file to begin the component

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,6 @@
+import "./styles/backdrop.css";
+const Backdrop = () => {
+  return <div>backdrop</div>;
+};
+
+export default Backdrop;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import { useEffect } from "react";
 import Loading from "../components/Loading.jsx";
 import Navbar from "../components/Navbar.jsx";
+import Backdrop from "../components/Backdrop.jsx";
 const Home = () => {
-  useEffect(()=> {
+  useEffect(() => {
     document.title = "Home - PokeAPI";
-  })
+  });
   return (
-    <>
+    <div className="fade-in">
       {/* <Loading /> */}
+      {/* <Backdrop /> */}
       <Navbar />
       <main>
         <h1>Home</h1>
       </main>
-    </>
+    </div>
   );
 };
 
