f4e9f7a938c4e62b25693ba8a8652acd0f84cd2c
tomikorzu <tomykorzu@icloud.com>
2024-10-02 11:20:21 -0300

    feat(index.css): add general styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 const App = () => {
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
