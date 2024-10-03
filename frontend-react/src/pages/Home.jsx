735e9d12e03b5502878e0a87dc26f0d6bf590f20
tomikorzu <tomykorzu@icloud.com>
2024-10-03 11:31:55 -0300

    feat(variables.js / Home.jsx): connect the api with the PokemonCards

--- frontend-react/src/pages/Home.jsx
+++ frontend-react/src/pages/Home.jsx
@@ -3,17 +3,49 @@ import Loading from "../components/Loading.jsx";
 import Navbar from "../components/Navbar.jsx";
 import { applyBlur } from "../utils/mainFunctions.js";
 import PokemonCard from "../components/PokemonCard.jsx";
+import { getApi } from "../utils/variables.js";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [pokemonsInfo, setPokemonsInfo] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(25);
+
   useEffect(() => {
     document.title = "Home - PokeAPI";
-  });
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const pokemons = await getApi(offset, limit);
+        setPokemonsInfo(pokemons);
+      } catch (error) {
+        console.error("Error fetching Pokémon data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [offset]);
+
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <div className="fade-in">
       <div className="back-drop">
         <Navbar />
         <main>
-          <div className="cards-container"></div>
+          <div className="cards-container">
+            {pokemonsInfo.map((pokemon) => (
+              <PokemonCard
+                key={pokemon.name}
+                name={pokemon.name}
+                image={pokemon.img}
+                type={pokemon.type}
+              />
+            ))}
+          </div>
         </main>
       </div>
     </div>
