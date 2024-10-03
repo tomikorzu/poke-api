d82d596e28cef790c7307f3491546ecc9c98f83d
tomikorzu <tomykorzu@icloud.com>
2024-10-03 18:03:32 -0300

    feat(main.js): add pokemon didnt found function

--- frontend/pokedex/main.js
+++ frontend/pokedex/main.js
@@ -4,20 +4,43 @@ import Loading from "../src/components/Loading.js";
 
 const search = document.getElementById("input-search");
 search.addEventListener("input", (event) => {
+  const found = searchPokemon(event);
+  noPokemonText(found);
+});
+
+const searchPokemon = (event) => {
   const { value } = event.target;
   const pokemonCards = document.querySelectorAll(".pokemon-card");
+  let found = false;
+
   pokemonCards.forEach((pokemonCard) => {
     const pokemonName = pokemonCard.querySelector(".card-name").textContent;
     if (pokemonName.includes(value)) {
       pokemonCard.style.display = "block";
+      found = true;
     } else {
       pokemonCard.style.display = "none";
     }
   });
-});
+  return found;
+};
+
+const noPokemonText = (found) => {
+  const noPokemon = document.querySelector(".no-pokemon");
+  if (!found) {
+    if (!noPokemon) {
+      const noPokemonMessage = document.createElement("h3");
+      noPokemonMessage.textContent = "No Pokémon found";
+      noPokemonMessage.className = "no-pokemon fade-in";
+      document.querySelector(".cards-container").appendChild(noPokemonMessage);
+    }
+  } else if (noPokemon) {
+    noPokemon.remove();
+  }
+};
 
 let offset = 0;
-let limit = 1000;
+let limit = 25;
 
 const { loadingElement, removeLoading } = Loading();
 
