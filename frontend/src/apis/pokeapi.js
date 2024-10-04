edb0f3c0d7dc4b509b44a351a370b9b12c5970bf
tomikorzu <tomykorzu@icloud.com>
2024-10-04 13:51:37 -0300

    feat(main.js / pokeapi.js): add tester

--- frontend/src/apis/pokeapi.js
+++ frontend/src/apis/pokeapi.js
@@ -1,42 +1,34 @@
-export const getApi = async (offset, limit) => {
+export const allPokemons = [];
+
+export const fetchData = async (offset, limit) => {
   const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
-  const pokemonsInfo = [];
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    const urls = data.results.map((pokemon) => pokemon.url);
-    await getApiOfPokemonsData(urls, pokemonsInfo);
-    return pokemonsInfo;
-  } catch (error) {
-    console.error("Error in fetching pokemons url:", error);
-    return;
-  }
+  const response = await fetch(url);
+  const data = await response.json();
+  let urls = await Promise.all(
+    data.results.map((pokemon) => {
+      return fetchPokemonData(pokemon.url);
+    })
+  );
+  return allPokemons;
 };
 
-const getApiOfPokemonsData = async (urls, pokemonsInfo) => {
-  for (const api of urls) {
-    try {
-      const response = await fetch(api);
-      const data = await response.json();
-      let img;
-      if (data.sprites.other.home.front_default) {
-        img = data.sprites.other.home.front_default;
-      } else if (data.sprites.other["official-artwork"].front_default) {
-        img = data.sprites.other["official-artwork"].front_default;
-      } else if (data.sprites.front_default) {
-        img = data.sprites.front_default;
-      } else {
-        img = "../src/assets/img/Pokeball.svg";
-      }
-      const pokemonData = {
-        img: img,
-        name: data.name,
-        types: data.types.map((type) => type.type.name),
+const fetchPokemonData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const newPokemon = {
+    name: data.name,
+    id: data.id,
+    image: data.sprites.other.home.front_default,
+    types: data.types.map((type) => type.type.name),
+    abilities: data.abilities.map((ability) => ability.ability.name),
+    stats: data.stats.map((stat) => {
+      return {
+        name: stat.stat.name,
+        base_stat: stat.base_stat,
       };
-      pokemonsInfo.push(pokemonData);
-    } catch (error) {
-      console.error("Error in fetching pokemons data:", err);
-    }
-  }
+    }),
+  };
+
+  allPokemons.push(newPokemon);
+  return allPokemons;
 };
-await getApi();
