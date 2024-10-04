70e439767bfa318ef92ec11c9d7173c48054dad9
tomikorzu <tomykorzu@icloud.com>
2024-10-04 18:52:27 -0300

    feat(Loading.js / main.js): implement the Loading component

--- frontend/pokedex/main.js
+++ frontend/pokedex/main.js
@@ -1,28 +1,47 @@
 import { fetchData, clearAllPokemons } from "../src/apis/pokeapi.js";
 import PokemonCard from "../src/components/PokemonCard.js";
+import Loading from "../src/components/Loading.js";
 
 let offset = 0;
 let limit = 25;
 let currentPage = 1;
+let currentRequest = 0;
 const pokemonsCardContainer = document.querySelector(".cards-container");
 const currentPageIndicator = document.querySelector(".current-page-h3");
 
 const updateFetch = async (offset, limit) => {
+  const requestId = ++currentRequest;
+
   pokemonsCardContainer.innerHTML = "";
+  clearAllPokemons();
 
-  clearAllPokemons()
+  const { loading, removeLoading } = Loading();
 
-  const pokemons = await fetchData(offset, limit);
+  try {
+    const pokemons = await fetchData(offset, limit);
 
-  if (pokemons) {
-    pokemons.map((pokemon) => {
-      PokemonCard({
-        name: pokemon.name,
-        img: pokemon.image,
-        types: pokemon.types,
+    if (requestId !== currentRequest) {
+      console.log(
+        `Request ${requestId} ignorada, currentRequest es ${currentRequest}`
+      );
+      return;
+    }
+
+    if (pokemons) {
+      pokemons.map((pokemon) => {
+        PokemonCard({
+          name: pokemon.name,
+          img: pokemon.image,
+          types: pokemon.types,
+        });
       });
-    });
+    }
+  } catch (error) {
+    console.error("There was an error:", error);
+  } finally{
+    removeLoading();
   }
+
   updateButtons();
 };
 
