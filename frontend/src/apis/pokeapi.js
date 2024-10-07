1b0cc8ad467d6941410da37e12a1163acc625f5f
tomikorzu <tomykorzu@icloud.com>
2024-10-06 22:39:27 -0300

    feat(main.js): resolve search pageSystem problem

--- frontend/src/apis/pokeapi.js
+++ frontend/src/apis/pokeapi.js
@@ -44,41 +44,11 @@ export const fetchPokemonsSearch = async (query) => {
     "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10000"
   );
   const data = await response.json();
-  
-  
+
   const names = data.results;
   const namesThatIncludesQuery = names.filter((name) => {
     return name.name.includes(query);
   });
 
   return namesThatIncludesQuery;
-};
-
-fetchPokemonsSearch("ar").then((response) => console.log(response));
-
-export const fetchPokemonByName = async (name) => {
-  try {
-    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      return null;
-    }
-    const data = await response.json();
-    return {
-      name: data.name,
-      id: data.id,
-      image:
-        data.sprites.other.home.front_default ||
-        "../src/assets/img/Pokeball.svg",
-      types: data.types.map((type) => type.type.name),
-      abilities: data.abilities.map((ability) => ability.ability.name),
-      stats: data.stats.map((stat) => ({
-        name: stat.stat.name,
-        base_stat: stat.base_stat,
-      })),
-    };
-  } catch (err) {
-    console.error("There was an error:", err);
-    return null;
-  }
 };
